ba98fa14d740e04c85c334f8669d35394dff784a
Бондарчук Дмитрий Валерьевич <DVBondarchuk@int.rolfcorp.ru>
2019-03-07 15:53:11 +0300

    1

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Paycheck {
     public static void main(String args[]) {
         double amountInAccount;
         amountInAccount = 128.57;
-        System.out.print("You earned $");
+        System.out.print("You earned $ and roubles");
         System.out.print(amountInAccount);
         System.out.println(" at work today.");
     }
