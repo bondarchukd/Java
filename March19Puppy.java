83dc78260df0dbac3823dc831cb192ba380365d2
Бондарчук Дмитрий Валерьевич <DVBondarchuk@int.rolfcorp.ru>
2019-03-19 16:42:02 +0300

    1

--- March19Puppy.java
+++ March19Puppy.java
@@ -2,6 +2,7 @@ public class March19Puppy{
 	int puppyAge;
 
 	public Puppy(String name){
+		//Пример конструктора с одним параметром name
 		System.out.printLn("Передаваемое имя: " + name);
 	}
 
@@ -11,12 +12,13 @@ public class March19Puppy{
 	}
 
 	public static void main(String[] args) {
+		//Создание нового объекта
 		Puppy myPuppy= new Puppy("Багет");
-		
+		//Вызов метода для установки возраста щенка
 		myPuppy.setAge(2);
-
+		//вызов метода для получения возраста
 		myPuppy.getAge();
-
+		//Получение переменной экземпляра класса
 		System.out.printLn("Значение переменной:" + myPuppy.puppyAge);
 
 	}
