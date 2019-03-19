91102a6d02993d8ce21829af6e98b89436b25835
Бондарчук Дмитрий Валерьевич <DVBondarchuk@int.rolfcorp.ru>
2019-03-19 16:56:24 +0300

    1

---
+++ March19.java
@@ -0,0 +1,17 @@
+class March19 {
+
+	//example of enum (перечисления)
+	enum FreshJuiceSize{ SMALL, MEDIUM, LARGE}
+	FreshJuiceSize size;
+
+	public class FreshJuiceTest {
+
+		public static void main(String[] args) {
+		FreshJuice juice = nre FreshJuice();
+		juice.size = FreshJuice.FreshJuiceSize.MEDIUM;
+		System.out.println("Size: " + juice.size);
+
+	}
+
+	
+}
