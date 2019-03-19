a90dbbee8f3bc4d768d3f19fdfa82ae37a55958e
Бондарчук Дмитрий Валерьевич <DVBondarchuk@int.rolfcorp.ru>
2019-03-19 08:27:18 +0300

    1

--- Home.java
+++ Home.java
@@ -1,8 +1,17 @@
-class Home {
+public class Home {
 	int a = 1;
 	int b =2;
 
 	public static void main(String[] args) {
-		
+		System.out.printLn("1");
+		System.out.printLn("Amigo ");
+		System.out.printLn("The ");
+		System.out.print("Best");
+		System.out.printLn("Risha & Amigo");
+	}
+	/* Example of comments*/
+	public static double pi() {
+		//one more example of comment
+		return 3.14;	
 	}
 }
