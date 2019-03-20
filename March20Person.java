98c349c7698e896f4c7f0e55db8c2be7d238c66e
Бондарчук Дмитрий Валерьевич <DVBondarchuk@int.rolfcorp.ru>
2019-03-20 09:03:09 +0300

    1

---
+++ March20Person.java
@@ -0,0 +1,15 @@
+public class March20Person {
+
+}
+
+public March20Person {
+
+	//example of constructor
+}
+
+public March20Person(String name) {
+
+this.name = name;
+//example of constructor that has parameter name
+
+}
