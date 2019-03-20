0622eca9dc8c87c90ce028279d3f822d6644728a
Бондарчук Дмитрий Валерьевич <DVBondarchuk@int.rolfcorp.ru>
2019-03-20 13:41:18 +0300

    1

---
+++ Array.java
@@ -0,0 +1,9 @@
+public class Array{
+
+	public static void main(String[] args) {
+		
+		int[] arr = {1, 2, 4, 56, 34, 675, 3, 0};
+
+		System.out.println("Determine min and max element: \n");
+	}
+}
