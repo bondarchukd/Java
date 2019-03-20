1bc2c372ced18911d2f81433db1d2e4107351004
Бондарчук Дмитрий Валерьевич <DVBondarchuk@int.rolfcorp.ru>
2019-03-20 12:41:44 +0300

    1

---
+++ March20PersonObject.java
@@ -0,0 +1,11 @@
+package 	Desktop.УЧЕБА.Java.Java
+
+public class March20PersonRunner{
+
+	public static void main(String[] args) {
+		
+		Person person = new March20Person("Ivanov");
+
+		System.out.println(person); 
+	}
+}
